0b8ae575a2eb992135aaf2c55e3ee068a127a712
I-the-Creator <andrey.burluckiy@gmail.com>
2022-08-20 12:39:28 +0200

    Home page added; Projects and Clients passed in

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import AddClientModal from '../components/AddClientModal/AddClientModal'
+import Clients from '../components/Clients/Clients'
+import Projects from '../components/Projects/Projects'
+
+const Home = () => {
+  return (
+    <>
+      <div className="d-flex gap-3 mb-4">
+        <AddClientModal />
+      </div>
+      <Projects />
+      <hr />
+      <Clients />
+    </>
+  )
+}
+
+export default Home
