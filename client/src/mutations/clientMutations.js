41f3b8e1f9be2c3301d4098b94bba2da073cc479
I-the-Creator <andrey.burluckiy@gmail.com>
2022-08-25 17:34:15 +0200

    Clients component with rows added

--- client/src/mutations/clientMutations.js
+++ client/src/mutations/clientMutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 
 const ADD_CLIENT = gql`
-  mutation addClient($name: String!, $email: String!, $phone: String!) {
+  mutation AddClient($name: String!, $email: String!, $phone: String!) {
     addClient(name: $name, email: $email, phone: $phone) {
       id
       name
@@ -13,7 +13,7 @@ const ADD_CLIENT = gql`
 
 const DELETE_CLIENT = gql`
   # set the data to be passed in and its type
-  mutation deleteClient($id: ID!) {
+  mutation DeleteClient($id: ID!) {
     # call the mutation with arguments and get values to return after
     deleteClient(id: $id) {
       id
