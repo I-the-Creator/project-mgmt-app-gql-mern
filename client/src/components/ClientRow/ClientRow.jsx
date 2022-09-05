61832afeb270123dfcac81e5a08edc9765d46de0
I-the-Creator <andrey.burluckiy@gmail.com>
2022-09-05 20:14:20 +0200

    query refetch used instead of cache update to rerender whole page

--- client/src/components/ClientRow/ClientRow.jsx
+++ client/src/components/ClientRow/ClientRow.jsx
@@ -4,23 +4,26 @@ import { useMutation } from '@apollo/client'
 
 import { DELETE_CLIENT } from '../../mutations/clientMutations'
 import { GET_CLIENTS } from '../../queries/clientQueries'
+import { GET_PROJECTS } from '../../queries/projectQueries'
 
 const ClientRow = ({ client }) => {
-  // define deleting client mutation and pass in arguments(values) whish will feed the mutation query - 'client id'
+  //* define deleting client mutation and pass in arguments(values) whish will feed the mutation query - 'client id'
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    // refetchQueries: [{ query: GET_CLIENTS }],
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({
-        query: GET_CLIENTS,
-      })
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      })
-    },
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }], // refetch queries as projects and clients have to be rerendered when client is deleted with it's projects
+
+    //* Update client on Client delete without updating list of projects
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({
+    //     query: GET_CLIENTS,
+    //   })
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   })
+    // },
   })
 
   return (
